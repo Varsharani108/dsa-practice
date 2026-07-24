0008358f4031110d8a8d5cf8ba62ee96fea032f6
Varsharani108 <Varsharanibgs108@gmail.com>
2026-07-24 23:00:01 +0530

    Solved Single Number using XOR

---
+++ Arrays/level1/singleNumber.java
@@ -0,0 +1,19 @@
+public class singleNumber {
+
+    public static int singleNumber(int[] arr) {
+        int xor = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            xor ^= arr[i];
+        }
+
+        return xor;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {4, 1, 2, 1, 2};
+
+        System.out.println(singleNumber(arr));
+    }
+}
