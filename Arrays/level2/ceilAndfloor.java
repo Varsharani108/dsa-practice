6310fca2ec00054f29edbe54c05a338582da73c3
Varsharani108 <Varsharanibgs108@gmail.com>
2026-07-27 12:36:48 +0530

    Solved Floor and Ceil using Binary Search

---
+++ Arrays/level2/ceilAndfloor.java
@@ -0,0 +1,50 @@
+package Arrays.level2;
+
+public class ceilAndfloor {
+    public static int floor(int[] arr, int target){
+         int low=0,high=arr.length-1,ans=-1;
+        
+        while (low <= high) {
+            int mid=(high+low)/2;
+            if(target >= arr[mid]){
+                
+                ans=mid;
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+               
+                
+            }
+    
+        }
+        return arr[ans];
+
+    }
+
+    public static int ceil(int[] arr, int target) {
+        int low=0,high=arr.length-1,ans=-1;
+        
+        while (low <= high) {
+            int mid=(high+low)/2;
+            if(target <= arr[mid]){
+                ans=mid;
+
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+                
+            }
+    
+        }
+        return arr[ans];
+
+    }
+    public static void main(String[] args) {
+        int arr[]={1,4,8,9,12,23};
+        System.out.println("Floor"+floor(arr,12));
+        System.out.println("Ceil"+ceil(arr,12));
+    }
+
+}
