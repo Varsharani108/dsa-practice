302df1e5e8319f621f8d0c188d61628387f9fc64
Varsharani108 <Varsharanibgs108@gmail.com>
2026-07-27 12:04:59 +0530

    Solved Search Insert Position using Binary Search

---
+++ Arrays/level2/searchInsertPosition.java
@@ -0,0 +1,28 @@
+package Arrays.level2;
+
+public class searchInsertPosition {
+    public static int search(int[] arr, int target){
+        int low=0,high=arr.length-1,ans=arr.length;
+        
+        while (low <= high) {
+            int mid=(high+low)/2;
+            if(target <= arr[mid]){
+                ans=mid;
+
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+                
+            }
+    
+        }
+        return ans;
+
+    }
+    public static void main(String[] args) {
+        int arr[]={1,4,8,9,12,23};
+        System.out.println(search(arr,12));
+        
+    }
+}
