655489234d41ab3f6ebb0ed6527555bd5b6e5635
Varsharani108 <Varsharanibgs108@gmail.com>
2026-07-30 01:27:03 +0530

    Add find peak element using binary search

---
+++ Arrays/level2/findPeakElement.java
@@ -0,0 +1,31 @@
+package Arrays.level2;
+
+public class findPeakElement {
+
+    public static int findPeak(int[] arr) {
+
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low < high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] < arr[mid + 1]) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+
+        }
+
+        return low;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {1,2,3,1};
+
+        System.out.println(findPeak(arr));
+    }
+}
