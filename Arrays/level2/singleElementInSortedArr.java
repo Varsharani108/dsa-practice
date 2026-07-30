af2307b2f3f29a5abab238adf8f705da11922b4c
Varsharani108 <Varsharanibgs108@gmail.com>
2026-07-30 22:25:07 +0530

    Solved Single Element in Sorted Array using Binary Search

---
+++ Arrays/level2/singleElementInSortedArr.java
@@ -0,0 +1,29 @@
+package Arrays.level2;
+
+public class singleElementInSortedArr {
+    public static int singleElement(int[] arr) {
+        int low = 0, high = arr.length - 1;
+        while (low < high) {
+            int mid = (high + low) / 2;
+
+            if (mid % 2 == 1) {
+                mid--;
+            }
+            if (arr[mid] == arr[mid + 1]) {
+                low = mid + 2;
+
+            } else {
+                high = mid;
+            }
+        }
+        return arr[low];
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = { 1, 1, 2, 3, 3, 4, 4, 8, 8 };
+
+        System.out.println(singleElement(arr));
+    }
+
+}
