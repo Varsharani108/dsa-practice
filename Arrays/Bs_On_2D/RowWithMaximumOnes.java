8b81e6755361db873bb41be8a7e3f3c9aa8782a4
Varsharani108 <Varsharanibgs108@gmail.com>
2026-08-25 23:53:51 +0530

    Add row with maximum ones using binary search

---
+++ Arrays/Bs_On_2D/RowWithMaximumOnes.java
@@ -0,0 +1,49 @@
+public class RowWithMaximumOnes {
+
+    public static int findRowWithMaxOnes(int[][] matrix) {
+
+        int maxOnes = 0;
+        int rowIndex = -1;
+
+        for (int i = 0; i < matrix.length; i++) {
+
+            int low = 0;
+            int high = matrix[i].length - 1;
+            int firstOne = matrix[i].length;
+
+            // Binary search for first 1
+            while (low <= high) {
+
+                int mid = low + (high - low) / 2;
+
+                if (matrix[i][mid] == 1) {
+                    firstOne = mid;
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+
+            int ones = matrix[i].length - firstOne;
+
+            if (ones > maxOnes) {
+                maxOnes = ones;
+                rowIndex = i;
+            }
+        }
+
+        return rowIndex;
+    }
+
+    public static void main(String[] args) {
+
+        int[][] matrix = {
+            {0, 0, 0, 1},
+            {0, 1, 1, 1},
+            {0, 0, 1, 1},
+            {0, 0, 0, 0}
+        };
+
+        System.out.println(findRowWithMaxOnes(matrix));
+    }
+}
