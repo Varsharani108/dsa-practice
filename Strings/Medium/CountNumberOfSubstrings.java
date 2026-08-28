9356f00f5d60d0fd2905c710634aafcf6071ca04
Varsharani108 <Varsharanibgs108@gmail.com>
2026-08-28 23:51:00 +0530

    Add count substrings with exactly k distinct characters

---
+++ Strings/Medium/CountNumberOfSubstrings.java
@@ -0,0 +1,57 @@
+package Strings.Medium;
+
+import java.util.HashMap;
+
+public class CountNumberOfSubstrings {
+
+    public static int countSubstrings(String s, int k) {
+
+        return atMostK(s, k) - atMostK(s, k - 1);
+    }
+
+    public static int atMostK(String s, int k) {
+
+        if (k == 0) {
+            return 0;
+        }
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        int left = 0;
+        int count = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+
+            char ch = s.charAt(right);
+
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+
+            // More than k distinct characters
+            while (map.size() > k) {
+
+                char leftChar = s.charAt(left);
+
+                map.put(leftChar, map.get(leftChar) - 1);
+
+                if (map.get(leftChar) == 0) {
+                    map.remove(leftChar);
+                }
+
+                left++;
+            }
+
+            // Number of valid substrings ending at right
+            count += right - left + 1;
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+        String s = "pqpqs";
+        int k = 2;
+
+        System.out.println(countSubstrings(s, k));
+    }
+}
