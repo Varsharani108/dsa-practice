ddba97ccd61861617139592a943b8480b3661122
Varsharani108 <Varsharanibgs108@gmail.com>
2026-08-30 23:47:07 +0530

    Add count and say solution

---
+++ Strings/Hard/CountAndSay.java
@@ -0,0 +1,44 @@
+package Strings.Hard;
+
+public class CountAndSay {
+
+    public static String countAndSay(int n) {
+
+        String result = "1";
+
+        for (int i = 2; i <= n; i++) {
+
+            StringBuilder next = new StringBuilder();
+
+            int count = 1;
+
+            for (int j = 1; j < result.length(); j++) {
+
+                if (result.charAt(j) == result.charAt(j - 1)) {
+                    count++;
+                } else {
+
+                    next.append(count);
+                    next.append(result.charAt(j - 1));
+
+                    count = 1;
+                }
+            }
+
+            // Add the last group
+            next.append(count);
+            next.append(result.charAt(result.length() - 1));
+
+            result = next.toString();
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        System.out.println(countAndSay(n));
+    }
+}
