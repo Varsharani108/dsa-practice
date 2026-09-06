db0e4e8076d97ad5518363a2ba10fa4118cb32dc
Varsharani108 <Varsharanibgs108@gmail.com>
2026-09-06 23:45:40 +0530

    Add binary strings without consecutive ones

---
+++ Recursion/Medium/GenerateBinaryStrings.java
@@ -0,0 +1,25 @@
+package Recursion.medium;
+
+public class GenerateBinaryStrings {
+
+    public static void generate(String str, int n) {
+
+        if (str.length() == n) {
+            System.out.println(str);
+            return;
+        }
+
+        generate(str + "0", n);
+
+        if (str.length() == 0 || str.charAt(str.length() - 1) != '1') {
+            generate(str + "1", n);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int n = 3;
+
+        generate("", n);
+    }
+}
