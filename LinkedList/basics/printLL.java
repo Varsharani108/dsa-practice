47197079b50c209861abaedeeb304ddebb8921d2
Varsharani108 <Varsharanibgs108@gmail.com>
2026-09-20 22:56:02 +0530

    Add print and addFirst linked list implementation

---
+++ LinkedList/basics/printLL.java
@@ -0,0 +1,39 @@
+package basics;
+
+public class printLL {
+
+    // Node
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    // Print Linked List
+    public static void printList(Node head) {
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+
+        // Creating nodes
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+        head.next.next.next = new Node(40);
+
+        // Print
+        printList(head);
+    }
+}
